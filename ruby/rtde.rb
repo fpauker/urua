62fbe2bd6a64b6e15b53969607f23639674c8d33
FlorianP <florian.pauker@gmail.com>
2019-05-10 14:13:01 +0200

    changed classes to modules

--- ruby/rtde.rb
+++ ruby/rtde.rb
@@ -1,84 +1,62 @@
 #!/usr/bin/env ruby
 require 'socket'        # Sockets are in standard library
 
-class Command
-  @rtde_request_protocol_version = 86        # ascii V
-  @rtde_get_urcontrol_version = 118           # ascii v
-  @rtde_text_message = 77                    # ascii m
-  @rtde_data_package = 85                    # ascii u
-  @rtde_control_package_setup_outputs = 79   # ascii o
-  @rtde_control_package_setup_inputs = 73    # ascii i
-  @rtde_control_package_start = 83           # ascii s
-  @rtde_control_package_pause = 80           # ascii p
-  class << self
-    attr_reader :rtde_request_protocol_version
-    attr_reader :rtde_get_urcontrol_version
-    attr_reader :rtde_text_message
-    attr_reader :rtde_data_package
-    attr_reader :rtde_control_package_setup_outputs
-    attr_reader :rtde_control_package_setup_inputs
-    attr_reader :rtde_control_package_start
-    attr_reader :rtde_control_package_pause
-  end
+module Command
+  RTDE_REQUEST_PROTOCOL_VERSION = 86        # ASCII V
+  RTDE_GET_URCONTROL_VERSION = 118           # ASCII V
+  RTDE_TEXT_MESSAGE = 77                    # ASCII M
+  RTDE_DATA_PACKAGE = 85                    # ASCII U
+  RTDE_CONTROL_PACKAGE_SETUP_OUTPUTS = 79   # ASCII O
+  RTDE_CONTROL_PACKAGE_SETUP_INPUTS = 73    # ASCII I
+  RTDE_CONTROL_PACKAGE_START = 83           # ASCII S
+  RTDE_CONTROL_PACKAGE_PAUSE = 80           # ascii p
 end
 
-#_RTDE_PROTOCOL_VERSION = 2
-
-class ConnectionState
-  @disconnected = 0
-  @connected = 1
-  @started = 2
-  @paused = 3
-  class << self
-    attr_reader :disconnected
-    attr_reader :connected
-    attr_reader :started
-    attr_reader :paused
-   end
+module ConnectionState
+  DISCONNECTED = 0
+  CONNECTED = 1
+  STARTED = 2
+  PAUSED = 3
 end
 
-#class RTDEException(Exception):
-#    def __init__(self, msg):
-#        self.msg = msg
-#    def __str__(self):
-#        return repr(self.msg)
+class RTDEException < RuntimeException; end
 
 class Rtde
+  RTDE_PROTOCOL_VERSION = 2
   def initialize(hostname, port)
     @hostname = hostname
     @port = port
-    @conn_state = ConnectionState.disconnected
+    @conn_state = ConnectionState::DISCONNECTED
     @sock = nil
     @output_config = nil
     @input_config = {}
   end
 
-  def hello
-    puts "hello"
-  end
-
   def connect
   #connect to robot controller using the rtde socket
-    if @sock
-      return
-    end
+    return if @sock
 
     @buf = 'b' # buffer data in binary format
     #self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #self.__sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     #self.__sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     #self.__sock.settimeout(DEFAULT_TIMEOUT)
-    @sock = TCPSocket.open(hostname, port)
-    @sock = ConnectionState.connected
+    @sock = TCPSocket.open(@hostname, @port)
+    @conn_state = ConnectionState::CONNECTED
   end
 
   def disconnect
     if @sock
       @sock.close
       @sock = nil
-      @conn_state = ConnectionState.disconnected
+      @conn_state = ConnectionState::DISCONNECTED
     end
   end
+
+  def is_connected
+    @conn_state
+  end
+
   def get_controller_version
     cmd = Command.rtde_get_urcontrol_version
   end
