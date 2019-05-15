98f48e7aeaf6d969f9f1f521ca1a92853d79ce6b
Florian <florian.pauker@gmail.com>
2019-05-15 21:09:45 +0200

    changes but still not working, problem with recv method

--- ruby/rtde.rb
+++ ruby/rtde.rb
@@ -3,8 +3,6 @@ require 'socket'        # Sockets are in standard library
 require 'logger'
 require_relative 'serialize'
 
-logger = Logger.new STDOUT
-
 module Command
   RTDE_REQUEST_PROTOCOL_VERSION = 86        # ASCII V
   RTDE_GET_URCONTROL_VERSION = 118           # ASCII V
@@ -28,6 +26,7 @@ class RtdeException < Exception; end
 class Rtde
   RTDE_PROTOCOL_VERSION = 2   #moved to Rtde class
   def initialize(hostname, port)
+    @logger = Logger.new STDOUT
     @hostname = hostname
     @port = port
     @conn_state = ConnectionState::DISCONNECTED
@@ -45,7 +44,9 @@ class Rtde
     #self.__sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     #self.__sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     #self.__sock.settimeout(DEFAULT_TIMEOUT)
-    @sock = TCPSocket.open(@hostname, @port)
+    @sock = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+    @sock.setsockopt Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1
+    @sock = TCPSocket.new(@hostname, @port)
     @conn_state = ConnectionState::CONNECTED
   end
 
@@ -65,9 +66,9 @@ class Rtde
     cmd = Command::RTDE_GET_URCONTROL_VERSION
     version = sendAndReceive cmd
     if version
-      logger.info 'Controller version' + version.major
+      @logger.info 'Controller version' + version.major
       if version.major == 3 && version.minor <=2 && version.bugfix < 19171
-        logger.error 'Upgrade your controller to version 3.2.19171 or higher'
+        @logger.error 'Upgrade your controller to version 3.2.19171 or higher'
         exit
       end
       [version.major, version.minor, version.bugfix, version.build]
@@ -87,7 +88,7 @@ class Rtde
     payload = variables.join ','
     result = sendAndReceive cmd, payload
     if types.length != 0 && result.types != types
-      logger.error(
+      @logger.error(
         'Data type inconsistency for input setup: ' +
         types.to_s + ' - ' +
         result.types.to_s
@@ -106,7 +107,7 @@ class Rtde
     payload = payload + variables.join(',')
     result = sendAndReceive cmd, payload
     if types.length != 0 and not result.types != types
-      logger.error(
+      @logger.error(
         'Data type inconsistency for output setup: ' +
         types.to_s + ' - ' +
         result.types.to_s
@@ -121,11 +122,11 @@ class Rtde
   def send_start
     cmd = Command::RTDE_CONTROL_PACKAGE_START
     if sendAndReceive cmd
-      logger.info 'RTDE synchronization started'
+      @logger.info 'RTDE synchronization started'
       @conn_state = ConnectionState::STARTED
       true
     else
-      logger.error 'RTDE synchronization failed to start'
+      @logger.error 'RTDE synchronization failed to start'
       false
     end
   end
@@ -134,21 +135,21 @@ class Rtde
     cmd = Command::RTDE_CONTROL_PACKAGE_PAUSE
     sucess = sendAndReceive(cmd)
     if success
-      logger.info 'RTDE synchronization paused'
+      @logger.info 'RTDE synchronization paused'
       @conn_state = ConnectionState::PAUSED
     else
-      logger.error('RTDE synchronization failed to pause')
+      @logger.error('RTDE synchronization failed to pause')
     end
     success
   end
 
   def send(input_data)
     if @conn_state != ConnectionState::STARTED
-      logger.error 'Cannot send when RTDE synchroinization is inactive'
+      @logger.error 'Cannot send when RTDE synchroinization is inactive'
       return
     end
     if not @input_config.key?(input_data.recipe_id)
-      logger.error 'Input configuration id not found: ' + @input_data.recipe_id
+      @logger.error 'Input configuration id not found: ' + @input_data.recipe_id
       return
     end
     config = @input_config[input_data.recipe_id]
@@ -177,24 +178,27 @@ class Rtde
     return unpack_start_package(payload)               if cmd == Command::RTDE_CONTROL_PACKAGE_START
     return unpack_pause_package(payload)               if cmd == Command::RTDE_CONTROL_PACKAGE_PAUSE
     return unpack_data_package(payload, output_config) if cmd == Command::RTDE_DATA_PACKAGE
-    logger.error 'Unknown package command' + cmd.to_s
+    @logger.error 'Unknown package command' + cmd.to_s
   end
 
-  def sendAndReceive(cmd, payload)
+  def sendAndReceive(cmd, payload = '')
     sendall(cmd, payload) ? recv(cmd) : nil
   end
 
-  def sendall(command, payload)
+  def sendall(command, payload = '')
     fmt = 'S>C'
     size = ([0,0].pack fmt).length + payload.length
-    if @sock
-      logger.error('Unable to send: not connected to Robot')
+    @buf = [size, command].pack(fmt) + payload
+    if !@sock
+      @logger.error 'Unable to send: not connected to Robot'
       return false
     end
 
     _, writable, _ = IO.select([], [@sock], [])
     if writable.length > 0
-      @sock.sendall(buf)
+      puts @buf
+      @sock.send(@buf,0)
+      puts 'send ok'
       true
     else
       trigger_disconnected
@@ -209,11 +213,14 @@ class Rtde
   end
 
   def recv(command)
+    p command
     while connected?
+      p 'conn'
       readable, _, xlist = IO.select([@sock], [], [@sock])
+      p readable
       if readable.length > 0
         more = @sock.recv(4096)
-        if len(more) == 0
+        if more.length == 0
           trigger_disconnected
           return nil
         end
@@ -221,27 +228,33 @@ class Rtde
       end
 
       if xlist.length > 0 || readable.length == 0
-        logger.info 'lost connection with controller'
+        @logger.info 'lost connection with controller'
         trigger_disconnected
         return nil
       end
 
       while @buf.length >= 3
+        @logger.info '@buf>=3'
         packet_header = Serialize::ControlHeader.unpack(@buf)
+
         if @buf.length >= packet_header.size
+          @logger.info '@buf.length >= packet_header.size'
           packet, @buf = @buf[3..packet_header.size], @buf[packet_header.size..-1]
+          puts packet
           data = on_packet(packet_header.command, packet)
+          puts data.to_s
           if @buf.length >= 3 && command == Command::RTDE_DATA_PACKAGE
+            @logger.info '@buf.length >= 3 && command == Command::RTDE_DATA_PACKAGE'
             next_packet_header = Serialize::ControlHeader.unpack(@buf)
             if next_packet_header.command == command
-              logger.info 'skipping package(1)'
+              @logger.info 'skipping package(1)'
               continue
             end
           end
           if packet_header.command == command
             return data
           else
-            logger.info 'skipping package(2)'
+            @logger.info 'skipping package(2)'
           end
         else
           break
@@ -252,16 +265,18 @@ class Rtde
   end
 
 	def trigger_disconnected
-		logger.info 'RTDE disconnected'
+		@logger.info 'RTDE disconnected'
 		disconnect
 	end
 
 	def unpack_protocol_version_package(payload)
+    @logger.info 'unpaking protocol version package'
 		return nil if payload.length != 1
 		Serialize::ReturnValue.unpack(payload).success
 	end
 
 	def unpack_urcontrol_version_package(payload)
+    @logger.info 'unpack urcontrol_version'
 		return nil if payload.length != 16
 		Serialize::ControlVersion.unpack payload
 	end
@@ -269,14 +284,14 @@ class Rtde
 	def unpack_text_message(payload)
 		return nil if payload.length < 1
 		msg = Serialize::Message.unpack payload
-		logger.error  (msg.source + ':' + msg.message) if msg.level == Serialize::Message::EXCEPTION_MESSAGE || msg.level == Serialize::Message::ERROR_MESSAGE
-		logger.warning(msg.source + ':' + msg.message) if msg.level == Serialize::Message::WARNING_MESSAGE
-		logger.info   (msg.source + ':' + msg.message) if msg.level == Serialize::Message::INFO_MESSAGE
+		@logger.error  (msg.source + ':' + msg.message) if msg.level == Serialize::Message::EXCEPTION_MESSAGE || msg.level == Serialize::Message::ERROR_MESSAGE
+		@logger.warning(msg.source + ':' + msg.message) if msg.level == Serialize::Message::WARNING_MESSAGE
+		@logger.info   (msg.source + ':' + msg.message) if msg.level == Serialize::Message::INFO_MESSAGE
 	end
 
 	def unpack_setup_outputs_package(payload)
 		if payload.length < 1
-			logger.error 'RTDE_CONTROL_PACKAGE_SETUP_OUTPUTS: No payload'
+			@logger.error 'RTDE_CONTROL_PACKAGE_SETUP_OUTPUTS: No payload'
 			return nil
 		end
 		Serialize::DataConfig.unpack_recipe payload
@@ -284,7 +299,7 @@ class Rtde
 
 	def unpack_setup_inputs_package(payload)
 		if payload.length < 1
-			logger.error 'RTDE_CONTROL_PACKAGE_SETUP_INPUTS: No payload'
+			@logger.error 'RTDE_CONTROL_PACKAGE_SETUP_INPUTS: No payload'
 			return nil
 		end
 		Serialize::DataConfig.unpack_recipe payload
@@ -292,7 +307,7 @@ class Rtde
 
 	def unpack_start_package(payload)
 		if payload.length != 1
-			logger.error 'RTDE_CONTROL_PACKAGE_START: Wrong payload size'
+			@logger.error 'RTDE_CONTROL_PACKAGE_START: Wrong payload size'
 			return nil
 		end
 		Serialize::ReturnValue.unpack(payload).success
@@ -300,7 +315,7 @@ class Rtde
 
 	def unpack_pause_package(payload)
 		if payload.length != 1
-			logger.error 'RTDE_CONTROL_PACKAGE_PAUSE: Wrong payload size'
+			@logger.error 'RTDE_CONTROL_PACKAGE_PAUSE: Wrong payload size'
 			return nil
 		end
 		Serialize::ReturnValue.unpack(payload).success
@@ -308,7 +323,7 @@ class Rtde
 
 	def unpack_data_package(payload)
 		if payload.length < 1
-			logger.error 'RTDE_DATA_PACKAGE: Missing output configuration'
+			@logger.error 'RTDE_DATA_PACKAGE: Missing output configuration'
 			return nil
 		end
 		output.config.unpack payload
--- ruby/test.rb
+++ ruby/test.rb
@@ -4,6 +4,13 @@ require 'logger'
 require_relative 'rtde'
 
 
-rtde = Rtde.new '192.168.56.101', 30004
-rtde.connect
-puts 'done!'
+con = Rtde.new '192.168.56.101', 30004
+con.connect
+puts con.connected?
+
+version = con.get_controller_version
+
+puts version.to_s
+p 'Disconnecting'
+con.disconnect
+puts con.connected?
